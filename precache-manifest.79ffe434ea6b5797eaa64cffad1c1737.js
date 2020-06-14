68fc7b77da359a7dd99b386c7926adcea1e2b4fd
Shreyas Ponkshe <shreyasponkshe12345@gmail.com>
2020-06-14 20:57:58 +0530

    Updates

---
+++ precache-manifest.79ffe434ea6b5797eaa64cffad1c1737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c22a1d6aae6ddb623f2de20277b5d2",
+    "url": "/yoga-pose-feedback/index.html"
+  },
+  {
+    "revision": "168ddebd3f5da9889b54",
+    "url": "/yoga-pose-feedback/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "7f110d49c370360aa85d",
+    "url": "/yoga-pose-feedback/static/css/main.65cec49f.chunk.css"
+  },
+  {
+    "revision": "168ddebd3f5da9889b54",
+    "url": "/yoga-pose-feedback/static/js/2.25baddcc.chunk.js"
+  },
+  {
+    "revision": "5e2252a167034352fa7bed0a873eaa75",
+    "url": "/yoga-pose-feedback/static/js/2.25baddcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f110d49c370360aa85d",
+    "url": "/yoga-pose-feedback/static/js/main.8d1646f9.chunk.js"
+  },
+  {
+    "revision": "ecb7d2f7ed82739b25a7",
+    "url": "/yoga-pose-feedback/static/js/runtime-main.189e8261.js"
+  }
+]);
